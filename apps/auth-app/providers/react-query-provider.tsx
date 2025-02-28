a20804ae81a00e11b13fec7958f871c7f4fab2a2
conny <conny@musma.net>
2025-02-28 16:59:25 +0900

    add devtools

--- apps/auth-app/providers/react-query-provider.tsx
+++ apps/auth-app/providers/react-query-provider.tsx
@@ -1,12 +1,17 @@
 "use client";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 type Props = {
   children: React.ReactNode;
 };
 const client = new QueryClient();
 const ReactQueryProvider = ({ children }: Props) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 export default ReactQueryProvider;
